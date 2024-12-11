335d094afb3e047b7ee24a0fb98b028363703ffa
SVM <SVM@DESKTOP-4CPJD5R>
2024-12-11 13:29:38 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async(context) =>
+    {
+        context.Response.ContentType = "text/html; charset=utf-8";
+        
+        await context.Response.SendFileAsync("wwwroot\\index.html");
+    });
+app.Run();
+
